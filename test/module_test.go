c9ff7ec570121ff4c5680238b1234650f26bf9e9
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-15 15:52:51 +0000

    initial unit tests - core attributes of most resources

--- test/module_test.go
+++ test/module_test.go
@@ -18,7 +18,13 @@ func TestModule(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	exampleName := terraform.Output(t, terraformOptions, "example_name")
+	repositoryName := terraform.Output(t, terraformOptions, "repository_name")
+	githubBranchProtectionPattern := terraform.Output(t, terraformOptions, "github_branch_protection_pattern")
+	githubRepositoryTag_protectionPattern := terraform.Output(t, terraformOptions, "github_repository_tag_protection_pattern")
+	githubActionsVariableValue := terraform.Output(t, terraformOptions, "github_actions_variable_value")
 
-	assert.Regexp(t, regexp.MustCompile(`^example-name*`), exampleName)
+	assert.Regexp(t, regexp.MustCompile(`^module-test-repo`), repositoryName)
+	assert.Regexp(t, regexp.MustCompile(`^main`), githubBranchProtectionPattern)
+	assert.Regexp(t, regexp.MustCompile(`^*`), githubRepositoryTag_protectionPattern)
+	assert.Regexp(t, regexp.MustCompile(`^test`), githubActionsVariableValue)
 }
