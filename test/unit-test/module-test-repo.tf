60a5ff43e3871df96c624122299d4b805873cbdd
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-15 13:55:41 +0000

    changes to test stack

--- test/unit-test/module-test-repo.tf
+++ test/unit-test/module-test-repo.tf
@@ -1,10 +1,14 @@
 module "module-test-repo" {
   source  = "ministryofjustice/repository/github"
-  version = "0.0.6"
+  version = "0.0.8"
 
   name            = "module-test-repo"
   description     = "THis repository was created by the terraform github repository unit test"
   homepage_url    = "https://github.com/ministryofjustice/terraform-github-repository"
   has_discussions = true
   topics          = ["operations-engineering"]
+  archive_on_destroy = false 
+  variables = {
+    TEST_VARIABLE = "test"
+  }
 }
--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -1,3 +1,52 @@
-output "example" {
-  value = module.module_test.name
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "github_branch_protection_pattern" {
+  value = module.module-test-repo.github_branch_protection.default.pattern
+}
+
+output "github_repository_tag_protection_pattern" {
+  value = module.module-test-repo.github_repository_tag_protection.default.pattern
+}
+
+output "github_actions_variable_value" {
+  value = module.module-test-repo.github_actions_variable["TEST_VARIABLE"].default.value
+}
+
+# Repository-Team Associations 
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
+}
+
+output "repository_name" {
+  value = module.module-test-repo.github_repository.default.name 
 }
