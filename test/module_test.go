cd334c9e89174844d7917a89b75b3981a03390b6
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-02-19 12:47:41 +0000

    update with test rteam 1 id

--- test/module_test.go
+++ test/module_test.go
@@ -33,7 +33,7 @@ func TestModule(t *testing.T) {
 	adminRepository := terraform.Output(t, terraformOptions, "admin_repository")
 	adminPermission := terraform.Output(t, terraformOptions, "admin_permission")
 
-	assert.Regexp(t, regexp.MustCompile(`test`), adminTeamId)
+	assert.Regexp(t, regexp.MustCompile(`9494611`), adminTeamId)
 	assert.Regexp(t, regexp.MustCompile(`module-test-repo`), adminRepository)
 	assert.Regexp(t, regexp.MustCompile(`admin`), adminPermission)
 }
