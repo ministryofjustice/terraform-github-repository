dcc42ef61b152773624d6e94acbd19905c989756
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-02-22 14:48:49 +0000

    Created test for a variable (github_token) and the outputs.

--- test/test_github_repositories.py
+++ test/test_github_repositories.py
@@ -3,39 +3,50 @@ import pytest
 from tftest import TerraformTest
 
 
-@pytest.fixture(scope='module')
-def tf_test():
+@pytest.fixture
+def plan():
     fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures/github_repos')
-    print(f"Fixtures Directory: {fixtures_dir}")
     tf = TerraformTest(tfdir=fixtures_dir)
     tf.setup(cleanup_on_exit=True)
-    yield tf
+    return tf.plan(output=True)
 
 
-def test_variables_presence(tf_test):
-    plan = tf_test.plan(output=True)
-    expected_variables = {
-        'name': 'github_token',
-        'description': 'Required by the GitHub Terraform provider',
-        'visibility': 'private',
-    }
-
-    for var_name, expected_value in expected_variables.items():
-        assert var_name in plan.variables, f"Variable '{var_name}' not found in plan."
-        assert plan.variables[var_name] == expected_value, f"Variable '{var_name}' does not have the expected value '{expected_value}'."
+def test_variables(plan):
+    assert 'github_token' in plan.variables
 
 
-def test_module_plan(tf_test):
-    plan = tf_test.plan(output=True)
-    print(f"Outputs: {plan.outputs}")
-    print(f"Variables: {plan.variables}")
-    assert False
-    # assert 'module.module-test-repo.github_repository.default' in plan.resources
-    # expected_teams = [
-    #     'operations_engineering_test',
-    #     'test_team_access',
-    #     'test_team_one',
-    #     'test_team_two'
-    #     ]
-    # for team in expected_teams:
-    #     assert f'data.github_team.{team}' in plan.data
+def test_terraform_outputs(plan):
+    outputs = plan.outputs
+
+    assert outputs['admin_permission'] == 'admin', \
+        "Admin permission should be 'admin'"
+    assert outputs['admin_repository'] == 'module-test-repo', \
+        "Admin repository should be 'module-test-repo'"
+    assert outputs['branch_protection_pattern'] == 'main', \
+        "Branch protection pattern should be 'main'"
+    assert outputs['description'] == \
+        'This repository was created by the terraform github repository unit test • This repository is defined and managed in Terraform', \
+        "Description value mismatch"
+    assert outputs['homepage_url'] == \
+        'https://github.com/ministryofjustice/terraform-github-repository', \
+        "Homepage URL should match"
+    assert outputs['maintain_permission'] == 'maintain', \
+        "Maintain permission should be 'maintain'"
+    assert outputs['maintain_repository'] == 'module-test-repo', \
+        "Maintain repository should be 'module-test-repo'"
+    assert outputs['name'] == 'module-test-repo', \
+        "Repository name should be 'module-test-repo'"
+    assert outputs['pull_permission'] == 'pull', \
+        "Pull permission should be 'pull'"
+    assert outputs['pull_repository'] == 'module-test-repo', \
+        "Pull repository should be 'module-test-repo'"
+    assert outputs['push_permission'] == 'push', \
+        "Push permission should be 'push'"
+    assert outputs['push_repository'] == 'module-test-repo', \
+        "Push repository should be 'module-test-repo'"
+    assert outputs['tag_protection_pattern'] == '*', \
+        "Tag protection pattern should be '*'"
+    assert outputs['test_actions_variable_value'] == 'test', \
+        "Test actions variable value should be 'test'"
+    assert outputs['visibility'] == 'public', \
+        "Visibility should be 'public'"
