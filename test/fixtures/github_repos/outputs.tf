e5df5a71fbbf680192ca4c002685adb9bc48541d
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-08-15 11:25:30 +0100

    uopdate unit tests

--- test/fixtures/github_repos/outputs.tf
+++ test/fixtures/github_repos/outputs.tf
@@ -22,10 +22,6 @@ output "branch_protection_pattern" {
   value = module.module-test-repo.branch_protection.pattern
 }
 
-output "tag_protection_pattern" {
-  value = module.module-test-repo.tag_protection.pattern
-}
-
 output "test_actions_variable_value" {
   value = module.module-test-repo.actions_variables["TEST_VARIABLE"].value
 }
